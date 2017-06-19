a3f5017f4891dde91c98d8d7ec0926c2b5005ad1
rooster0103 <kylebomar@hotmail.com>
2017-06-19 11:27:07 -0500

    Add files via upload

--- proj01/proj01.py
+++ proj01/proj01.py
@@ -1,10 +1,12 @@
-# Name:
-# Date:
+# Name:Kyle Bomar
+# Date: January 27, 2003
 
 # proj01: A Simple Program
 # This program asks the user for his/her name and age.
 # Then, it prints a sentence that says when the user will turn 100.
 
 # If you complete extensions, describe your extensions here!
-
-
+x = raw_input('Enter your name')
+n =int(raw_input("Enter your age"))
+b = 100 - n
+f =
