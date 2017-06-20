192aa4096a6f75baec62573c4198bb6b2a93f799
rooster0103 <kylebomar@hotmail.com>
2017-06-20 15:30:07 -0500

    Add files via upload

--- proj01/proj01.py
+++ proj01/proj01.py
@@ -1,31 +1,10 @@
-# Name:Kyle Bomar
-# Date: June 19, 2017
+# Name:
+# Date:
 
 # proj01: A Simple Program
 # This program asks the user for his/her name and age.
 # Then, it prints a sentence that says when the user will turn 100.
 
 # If you complete extensions, describe your extensions here!
-x = raw_input('Enter your name')
-n = int(raw_input('Enter your age'))
-
-b = 100-n
-f = 2017 +b
-hat = f-1
-
-
-m = raw_input("Have you already had a Birthday?")
-if  m == "no":
-    print hat
-
-
-if  m == "yes":
-
-    print f
-
-
-
-
-
 
 
--- proj04/proj04.py
+++ proj04/proj04.py
@@ -1,5 +1,5 @@
-# Name:
-# Date:
+# Name:Kyle Bomar
+# Date: 6/20/17
 
 """
 proj04
@@ -8,3 +8,19 @@ Asks the user for a string and prints out whether or not the string is a palindr
 
 """
 
+x = raw_input('Please insert a string:')
+alist=[]
+reverse_list=[]
+for letter in x:
+    alist.append(letter)
+
+y=len(alist)
+hat=x
+for letter in range(1,y+1):
+    reverse_list.append(x[-1])
+    x=x[:-1]
+
+
+
+
+if hat == reverse
